acbc970f6df4dc29ae9ed03c1e21f1beec6621eb
TM-Tareq <tmtareq20@gmail.com>
2025-01-21 17:40:52 +0600

    todays code

--- prbolem solve/print.cpp
+++ prbolem solve/print.cpp
@@ -1,1 +1,16 @@
-cpp
+#include<bits/stdc++.h>
+using namespace std;
+void print(int n) {
+    for(int i = 1; i <= n; i++) {
+        cout << i << " ";
+    }
+}
+int main() {
+
+    int n;
+    cin >> n;
+
+    print(n);
+
+    return 0;
+}
